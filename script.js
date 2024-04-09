edba4c857a7e6b3d0eec0fd8a19c37ab5afeaa2e
adithya <adithya.3035@gmail.com>
2024-04-09 15:36:26 -0700

    worked on saving data locally

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const value = document.getElementById("value");
 
 function updateCalories() {
   value.innerHTML = calGoal.value + " calories";
+  saveData();
 }
 
 button1.addEventListener("click", updateCalories);
@@ -14,6 +15,49 @@ const value2 = document.getElementById("value2");
 
 function updateProtein() {
   value2.innerHTML = proGoal.value + " grams";
+  saveData();
 }
 
 button2.addEventListener("click", updateProtein);
+
+function saveData() {
+  localStorage.setItem("data", value.innerHTML);
+  localStorage.setItem("data2", value2.innerHTML);
+}
+
+function showGoal() {
+  value.innerHTML = localStorage.getItem("data");
+  value2.innerHTML = localStorage.getItem("data2");
+}
+showGoal();
+
+// Lunch Functions
+
+const inputBoxLunch = document.getElementById("input-lunch");
+const listContainer = document.getElementById("list-container");
+
+function addTask() {
+  if (inputBoxLunch.value === "") {
+    alert("You must write something!");
+  } else {
+    let li = document.createElement("li");
+    li.innerHTML = inputBoxLunch.value;
+    listContainer.appendChild(li);
+    let span = document.createElement("span");
+    span.innerHTML = "\u00d7";
+    li.appendChild(span);
+  }
+  inputBox.value = "";
+  saveInfo();
+}
+
+listContainer.addEventListener(
+  "click",
+  function (e) {
+    if (e.target.tagName === "SPAN") {
+      e.target.parentElement.remove();
+      saveInfo();
+    }
+  },
+  false
+);
